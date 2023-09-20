0a4aef3c987affc2bcfddfb78bc25c03f84bd9e6
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-09-20 12:52:06 +0530

    meeting

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
 
 
-
-const nodeExternals = require('webpack-node-externals');
-
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = {
-  ...nextConfig,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals = [nodeExternals()];
-    }
-
-    return config;
-  },
-};
